5128d8518fc1579ad13e2909e6e658b27520f908
IvanCS719 <ivancupil17@gmail.com>
2023-06-18 10:05:15 -0600

    Fucionando admin-diego con main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/seccDiccChoco'
@@ -26,32 +25,3 @@ function App() {
 }
 
 export default App
-=======
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card/seccDiccChoco'
-import { Routes, Route } from 'react-router-dom'
-import FormAdmin from './components/formularios/formAdminAdd'
-import FormPublic from './components/formularios/formPublic'
-
-function App() {
-  return (
-    <>
-       <Routes>
-        <Route path='/' element={<Card></Card>}>  
-        </Route>
-        <Route path='/loginDicc' element={<LoginAdmin></LoginAdmin>}>   
-        </Route>
-        <Route path='/formulario' element={<FormAdmin></FormAdmin>}>   
-        </Route>
-        <Route path='/colaborar' element={<FormPublic></FormPublic>}>   
-        </Route>
-      </Routes>
-      
-     
-    </>
-  )
-}
-
-export default App
->>>>>>> admin-diego
