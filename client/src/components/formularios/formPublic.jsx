d2ab4464c762878f5dc011b7b4e3b9001b08c0cb
IvanCS719 <ivancupil17@gmail.com>
2023-06-24 14:23:53 -0600

    Actualización

--- client/src/components/formularios/formPublic.jsx
+++ client/src/components/formularios/formPublic.jsx
@@ -5,7 +5,7 @@ import NavBar from '../navbars/navbar';
 
 const FormField = ({ label, name, placeholder, errors, type = 'text' }) => (
     <div className='text-left mb-5'>
-        <label htmlFor={name}>{label}</label>
+        <label className='font-bold text-gray-800' htmlFor={name}>{label}</label>
         <Field
             type={type}
             id={name}
@@ -43,7 +43,7 @@ const Formulario = () => {
             //addEjemplos();
 
 
-           // console.log('newDataChoco:', dataChoco);
+            // console.log('newDataChoco:', dataChoco);
 
             //Convertir los arreglos de ejemplos a una string
 
@@ -52,8 +52,9 @@ const Formulario = () => {
 
             // Agregar las cadenas de texto al objeto values
             values.significado = values.significado ? values.significado : 'No Aplica';
-            values.acepciones = values.acepciones ? values.acepciones: 'No Aplica';
+            values.acepciones = values.acepciones ? values.acepciones : 'No Aplica';
             values.sinonimos = values.sinonimos ? values.sinonimos : 'No Aplica';
+            values.colaborador = values.colaborador ? values.colaborador : 'Anónimo';
             values.ejemplo_choco = dataChocoString;
             values.ejemplo_neutro = '';
 
@@ -126,96 +127,96 @@ const Formulario = () => {
     return (
         <div className='w-full min-h-screen'>
             <NavBar mfLogo={"MercadoFácil.mx"} mfLink={"https://mercadofacil.mx/"} cola={"Volver"} colaLink={"/"}
-            verDicc={"Inicio"} verDiccLink={'/'} masInfo={"Saber más"} masInfoLink={"https://mercadofacil.mx/las-palabras-del-choco/"}/>
+                verDicc={"Inicio"} verDiccLink={'/'} masInfo={"Saber más"} masInfoLink={"https://mercadofacil.mx/las-palabras-del-choco/"} />
 
-           <div className='w-full px-4 mb-2 md:px-6 flex flex-col items-center'>
-           <p className='mb-4 mt-3 font-semibold text-mfColor text-4xl'>¡Gracias por contribuir en la mejora de este Diccionario!</p>
-            
-            <>
-                <Formik
-                    //almacena los valores de cada campo
-                    initialValues={{
-                        palabra: '',
-                        significado: '',
-                        //significadoIng: '',
-                        acepciones: '',
-                        //acepcionesIng: '',
-                        sinonimos: '',
-                        //sinonimosIng: '',
-                        como_se_usa: '',
-                        //como_se_usa_Ing: '',
-                        titleEjemplo: '',
-                        EjemploChoco: '',
-                        id_categoria: 57,
-                        id_tipo: 1,
-                        colaborador: '',
-                        correo_electronico:''
+            <div className='w-full px-4 mb-2 md:px-6 flex flex-col items-center'>
+                <p className='mb-4 mt-4 font-semibold text-mfColor text-3xl'>¡Gracias por contribuir en la mejora de este Diccionario!</p>
 
-                    }}
-                    //validar que los valores escritos dentro del campo, correspondan a lo solicitado en cada tabla
-                    validate={(valores) => {
-                        let errores = {};
+                <>
+                    <Formik
+                        //almacena los valores de cada campo
+                        initialValues={{
+                            palabra: '',
+                            significado: '',
+                            //significadoIng: '',
+                            acepciones: '',
+                            //acepcionesIng: '',
+                            sinonimos: '',
+                            //sinonimosIng: '',
+                            como_se_usa: '',
+                            //como_se_usa_Ing: '',
+                            titleEjemplo: '',
+                            EjemploChoco: '',
+                            id_categoria: 57,
+                            id_tipo: 1,
+                            colaborador: '',
+                            correo_electronico: ''
 
-                        //valores de palabra
-                        if (!valores.palabra) {
-                            errores.palabra = 'Campo obligatorio*'
-                        }
+                        }}
+                        //validar que los valores escritos dentro del campo, correspondan a lo solicitado en cada tabla
+                        validate={(valores) => {
+                            let errores = {};
 
-                        if (!valores.significado) {
-                            errores.significado = 'Campo obligatorio*'
-                        }
-                        //valores de significado
-                        if (!valores.como_se_usa) {
-                            errores.como_se_usa = 'Campo obligatorio*'
-                        }
-
-
-                        //valores de acepsiones
-                        /*if (!valores.acepciones) {
-                            errores.acepciones = 'ingrese una palabra'
-                        } else if (!/^[a-zA-Z\s.,;:?!¡¿()"'-]+$/.test(valores.acepciones)) {
-                            errores.acepciones = 'solo puedes escribir palabras'
-                        }*/
-
-                        //valores de sinónimos
-                        /*if (!valores.sinonimos) {
-                            errores.sinonimos = 'ingrese una palabra'
-                        } else if (!/^[a-zA-Z\s.,;:?!¡¿()"'-]+$/.test(valores.sinonimos)) {
-                            errores.sinonimos = 'solo puedes escribir palabras'
-                        }*/
-
-                        //valores de como se usa
-                        /*if (!valores.como_se_usa) {
-                            errores.como_se_usa = 'Campo obligatorio'
-                        } else if (!/^[a-zA-Z\s.,;:?!¡¿()"'-]+$/.test(valores.como_se_usa)) {
-                            errores.como_se_usa = 'solo puedes escribir palabras'
-                        }*/
-
-                        //valores de ejemplo neutro
-
-                        if (arrTama.length == 0) {
-                            errores.titleEjemplo = 'Debe agregar almenos un ejemplo*'
-                        }
-
-                        arrTama.map((item, index) => {
-                            //valores de ejemplo choco
-                            if (!dataChoco[index]) {
-                                errores.EjemploChoco = 'Queda algún campo vacio en los ejemplos*'
+                            //valores de palabra
+                            if (!valores.palabra) {
+                                errores.palabra = 'Campo obligatorio*'
                             }
-                        })
+
+                            if (!valores.significado) {
+                                errores.significado = 'Campo obligatorio*'
+                            }
+                            //valores de significado
+                            if (!valores.como_se_usa) {
+                                errores.como_se_usa = 'Campo obligatorio*'
+                            }
+
+
+                            //valores de acepsiones
+                            /*if (!valores.acepciones) {
+                                errores.acepciones = 'ingrese una palabra'
+                            } else if (!/^[a-zA-Z\s.,;:?!¡¿()"'-]+$/.test(valores.acepciones)) {
+                                errores.acepciones = 'solo puedes escribir palabras'
+                            }*/
+
+                            //valores de sinónimos
+                            /*if (!valores.sinonimos) {
+                                errores.sinonimos = 'ingrese una palabra'
+                            } else if (!/^[a-zA-Z\s.,;:?!¡¿()"'-]+$/.test(valores.sinonimos)) {
+                                errores.sinonimos = 'solo puedes escribir palabras'
+                            }*/
+
+                            //valores de como se usa
+                            /*if (!valores.como_se_usa) {
+                                errores.como_se_usa = 'Campo obligatorio'
+                            } else if (!/^[a-zA-Z\s.,;:?!¡¿()"'-]+$/.test(valores.como_se_usa)) {
+                                errores.como_se_usa = 'solo puedes escribir palabras'
+                            }*/
+
+                            //valores de ejemplo neutro
+
+                            if (arrTama.length == 0) {
+                                errores.titleEjemplo = 'Debe agregar almenos un ejemplo*'
+                            }
+
+                            arrTama.map((item, index) => {
+                                //valores de ejemplo choco
+                                if (!dataChoco[index]) {
+                                    errores.EjemploChoco = 'Queda algún campo vacio en los ejemplos*'
+                                }
+                            })
 
 
 
-                        return errores;
-                    }}
-                    //para enviar formulario
-                    onSubmit={handleSubmit}
-                >
-                    {({ values, errors }) => (
-                        <Form className='max-w-max p-5 mt-3 bg-white rounded-2xl border-2 border-solid border-mfColor shadow-mfBoxShadow'>
-                            <h2 className='mb-4 font-semibold text-mfColor text-3xl'>Aportar Palabra</h2>
-                            <div className='w-full flex flex-col items-center'>
-                                
+                            return errores;
+                        }}
+                        //para enviar formulario
+                        onSubmit={handleSubmit}
+                    >
+                        {({ values, errors }) => (
+                            <Form className='max-w-max p-5 mt-3 bg-white rounded-2xl border-2 border-solid border-mfColor shadow-mfBoxShadow'>
+                                <h2 className='mb-6 font-semibold text-mfColor text-3xl'>Aportar Palabra</h2>
+                                <div className='w-full flex flex-col items-center'>
+
                                     <div className='w-auto flex flex-col md:flex-row gap-1 md:gap-5'>
                                         <FormField
                                             label="Palabra (requerido):"
@@ -230,24 +231,24 @@ const Formulario = () => {
                                             errors={errors}
                                         />
 
-                                       
+
 
                                     </div>
                                     <div className='w-auto flex flex-col items-center gap-1 mb-3'>
-                                        <p>Ejemplo de cómo usa la palabra (requerido):</p>
+                                        <p className='font-bold text-gray-800'>Ejemplo de cómo usa la palabra (requerido):</p>
                                         <ErrorMessage name="titleEjemplo" component={() => (
-                                                <div className='error text-red-600 font-medium'>{errors.titleEjemplo}</div>
-                                            )} />
-                                    <button type="button" className='max-w-max h-min rounded-md bg-mfColor px-3 py-1.5 text-white shadow-md font-medium' onClick={newEjemplos}><i className="fa-solid fa-plus"></i> Nuevo Ejemplo</button>
+                                            <div className='error text-red-600 font-medium'>{errors.titleEjemplo}</div>
+                                        )} />
+                                        <button type="button" className='max-w-max h-min rounded-md bg-mfColor px-3 py-1.5 text-white shadow-md font-medium' onClick={newEjemplos}><i className="fa-solid fa-plus"></i> Nuevo Ejemplo</button>
                                         <div className=''>
                                             <label htmlFor='titleEjemplo'>Ejemplos Agregados: <span className='font-bold'>{`${arrTama.length}`}</span></label>
-                                            
+
                                             <ErrorMessage name="EjemploChoco" component={() => (
                                                 <div className='error text-red-600 font-medium'>{errors.EjemploChoco}</div>
                                             )} />
 
                                         </div>
-                                        
+
                                     </div>
 
                                     <div className='w-full max-h-52 flex flex-col items-center overflow-auto mb-2'>
@@ -272,7 +273,7 @@ const Formulario = () => {
                                                 <button type="button" title='Eliminar este ejemplo' className='w-auto my-auto h-min rounded-md bg-mfColor px-3 py-1.5 text-white shadow-md font-medium' onClick={() => {
 
                                                     try {
-                                                        
+
 
                                                         const newDataChoco = [...dataChoco]; // Copia el arreglo original
                                                         newDataChoco.splice(index, 1); // Realiza la modificación en la copia
@@ -290,8 +291,8 @@ const Formulario = () => {
 
                                     </div>
                                     <div className='w-auto flex flex-col md:flex-row gap-1 md:gap-5'>
-                                        
-                                    <FormField
+
+                                        <FormField
                                             label="¿En qué casos se usa? (requerido):"
                                             name="como_se_usa"
                                             placeholder="Explique brevemente"
@@ -309,45 +310,55 @@ const Formulario = () => {
 
 
                                     <div className='w-auto flex flex-col md:flex-row gap-1 md:gap-5'>
-                                        <FormField
-                                            label="Nombre:"
-                                            name="colaborador"
-                                            placeholder="Ingrese su nombre"
-                                        // errors={errors}
-                                        />
+                                        <div className='max-w-min'>
+                                            <FormField
+                                                label="Nombre:"
+                                                name="colaborador"
+                                                placeholder="Ingrese su nombre"
+                                            // errors={errors}
 
-                                        <FormField
-                                            label="Correo electrónico:"
-                                            name="correo_electronico"
-                                            placeholder="Ingrse su correo electronico"
-                                        // errors={errors}
-                                        />
+                                            />
+                                            <p className='text-sm text-left -mt-3'>Opcional: Si dejas el nombre vacío tu contribución al diccionario será anónima.</p>
+                                        </div>
+
+
+
+                                        <div className='max-w-min'>
+                                            <FormField
+                                                label="Correo electrónico:"
+                                                name="correo_electronico"
+                                                placeholder="Ingrse su correo electronico"
+                                                type='email'
+                                            // errors={errors}
+                                            />
+                                            <p className='text-sm text-left -mt-3'>Opcional: Si ingresas tu correo electrónico te avisaremos cuando tu aportación sea aprobada o rechazada.</p>
+                                        </div>
                                     </div>
-         
-                                    
 
 
-                            </div>
 
-                            <button type='submit' className='w-auto rounded-md mt-2 bg-mfColor px-5 py-1.5 text-white shadow-md font-medium'><i className="fa-solid fa-paper-plane"></i> Enviar</button>
-                            <div
-                                className={`fixed bg-modal inset-0 flex items-center justify-center transition-all duration-200 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-                                    }`}
-                            >
-                                <div className="bg-white mx-4 sm:w-96 p-5 rounded-xl shadow-mfBoxShadow border">
-                                    <p className="text-2xl text-gray-800 font-bold mb-3">¡Gracias!</p>
-                                    <p className='text-8xl mb-2 text-green-600'><i className="fa-regular fa-circle-check"></i></p>
-                                    <p className="text-lg text-gray-700 font-medium mb-4">Su aportación ha sido enviada para su revisión y posterior anexión al diccionario del choco.</p>
-                                    <button type="button" className='w-auto h-min rounded-md bg-mfColor px-3 py-1.5 text-white shadow-md font-medium' onClick={closeModal}>Aceptar</button>
+
                                 </div>
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
 
-            </>
-            
-           </div>
+                                <button type='submit' className='w-auto rounded-md mt-6 bg-mfColor px-5 py-1.5 text-white shadow-md font-medium'><i className="fa-solid fa-paper-plane"></i> Enviar</button>
+                                <div
+                                    className={`fixed bg-modal inset-0 flex items-center justify-center transition-all duration-200 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                                        }`}
+                                >
+                                    <div className="bg-white mx-4 sm:w-96 p-5 rounded-xl shadow-mfBoxShadow border">
+                                        <p className="text-2xl text-gray-800 font-bold mb-3">¡Gracias!</p>
+                                        <p className='text-8xl mb-2 text-green-600'><i className="fa-regular fa-circle-check"></i></p>
+                                        <p className="text-lg text-gray-700 font-medium mb-4">Su aportación ha sido enviada para su revisión y posterior anexión al diccionario del choco.</p>
+                                        <button type="button" className='w-auto h-min rounded-md bg-mfColor px-3 py-1.5 text-white shadow-md font-medium' onClick={closeModal}>Aceptar</button>
+                                    </div>
+                                </div>
+                            </Form>
+                        )}
+                    </Formik>
+
+                </>
+
+            </div>
         </div>
     );
 }
